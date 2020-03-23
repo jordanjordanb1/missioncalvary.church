215bd8a53ef21bf96544976fbc0299771aeea5f1
Jordan Barbosa <jordansbarbosa01@gmail.com>
2020-03-22 23:10:13 -0400

    Fixed importing for about section

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,15 @@ import React from "react"
 import HomeLayout from '../layouts/HomeLayout'
 import SEO from "../components/shared/seo"
 import Location from "../components/Index/Location/Location"
-import About from "../components/About/About"
+import About from "../components/Index/About/About"
+import Contact from "../components/Index/Contact/Contact"
 
 const IndexPage = () => (
   <HomeLayout>
     <SEO title="Home" />
     <About />
     <Location />
+    <Contact />
   </HomeLayout>
 )
 
