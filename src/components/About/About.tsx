a6c68bd4568121cb892d5122fbfb2e2ba709fc82
Jordan Barbosa <jordansbarbosa01@gmail.com>
2020-03-22 23:01:54 -0400

    Created about section

---
+++ src/components/About/About.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import './About.scss'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Image from './Image/Image'
+
+export default function About() {
+  return (
+    <Row as="section" className="about" style={{ background: 'white' }}>
+      <Col xs="12" lg="4">
+        <Image />
+      </Col>
+      <Col xs="12" lg="8" className="p-2 d-flex align-items-center text-center">
+        <div className="about-wrapper">
+          <Row noGutters>
+            <Col xs="12" lg={{ span: 6, offset: 3 }}>
+              <h1>Who we are</h1>
+            </Col>
+          </Row>
+
+          <Row>
+            <Col xs="12" className="pr-5 pl-5">
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
+                perferendis temporibus commodi quidem magnam ullam corrupti
+                voluptas rerum, accusantium nihil expedita sit, velit eveniet
+                sequi quo? Recusandae temporibus consequuntur dolorum. Lorem
+                ipsum dolor sit amet consectetur adipisicing elit. Odit
+                perferendis temporibus commodi quidem magnam ullam corrupti
+                voluptas rerum, accusantium nihil expedita sit, velit eveniet
+                sequi quo? Recusandae temporibus consequuntur dolorum. Lorem
+                ipsum dolor sit amet consectetur adipisicing elit. Odit
+                perferendis temporibus commodi quidem magnam ullam corrupti
+                voluptas rerum, accusantium nihil expedita sit, velit eveniet
+                sequi quo? Recusandae temporibus consequuntur dolorum. Lorem
+                ipsum dolor sit amet consectetur adipisicing elit. Odit
+                perferendis temporibus commodi quidem magnam ullam corrupti
+                voluptas rerum, accusantium nihil expedita sit, velit eveniet
+                sequi quo? Recusandae temporibus consequuntur dolorum.
+              </p>
+            </Col>
+          </Row>
+        </div>
+      </Col>
+    </Row>
+  )
+}
---
+++ src/components/About/Image/Image.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Image.scss'
+
+export default function Image() {
+  return (
+    <div className="about-image d-flex justify-content-center align-items-center">
+      <img src="https://dummyimage.com/600x400/000/fff" alt="Fake img" />
+    </div>
+  )
+}
