92df9039ca1fa33e25bd984519aca5532ae97104
Jordan Barbosa <jordansbarbosa01@gmail.com>
2020-03-22 23:02:21 -0400

    Added about section

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,12 @@ import React from "react"
 import HomeLayout from '../layouts/HomeLayout'
 import SEO from "../components/shared/seo"
 import Location from "../components/Location/Location"
+import About from "../components/About/About"
 
 const IndexPage = () => (
   <HomeLayout>
     <SEO title="Home" />
+    <About />
     <Location />
   </HomeLayout>
 )
