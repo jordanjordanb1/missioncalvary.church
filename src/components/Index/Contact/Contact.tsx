dac3b93ddf4045a2b701eefa7c7fca05bdf404ad
Jordan Barbosa <jordansbarbosa01@gmail.com>
2020-03-23 23:33:16 -0400

    Created contact section

---
+++ src/components/Index/Contact/Contact.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Contact.scss'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import ContactForm from './ContactForm/ContactForm'
+
+export default function Contact() {
+  return (
+    <Row as="section" className="contact-section">
+      <Col
+        xs="12"
+        md={{ span: 8, offset: 2 }}
+        lg={{ span: 6, offset: 3 }}
+        className="p-5"
+      >
+        <h1 className="mb-4 text-center">Contact us</h1>
+
+        <ContactForm />
+      </Col>
+    </Row>
+  )
+}
