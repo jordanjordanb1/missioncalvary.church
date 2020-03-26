84815d9cc314276d44b40fccb7e05c21d79a2472
Jordan Barbosa <jordansbarbosa01@gmail.com>
2020-03-25 22:48:48 -0400

    Started about page

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,21 +1,18 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import Container from 'react-bootstrap/Container'
 import Footer from '../components/shared/footer/Footer'
 import Header from '../components/shared/header/Header'
 
-const Layout = ({ children }) => {
+const Layout = ({ children }: any) => {
   return (
     <>
       <Header />
-      <Container as="main">{children}</Container>
+      <Container fluid as="main">
+        {children}
+      </Container>
       <Footer />
     </>
   )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
 }
 
 export default Layout
---
+++ src/pages/about.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Layout from '../layouts/Layout'
+import SEO from '../components/shared/seo'
+
+export default function AboutPage() {
+  return (
+    <Layout>
+      <SEO title="About" />
+    </Layout>
+  )
+}
